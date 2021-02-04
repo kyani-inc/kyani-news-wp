33b251e2a77c3f6c37fad227d6d4be22910febe9
Victor Santiago <vsantiago@kyanicorp.com>
2021-02-04 10:05:25 -0700

    added secondary menu option

--- themes/understrap/inc/setup.php
+++ themes/understrap/inc/setup.php
@@ -47,6 +47,7 @@ if ( ! function_exists( 'understrap_setup' ) ) {
 		register_nav_menus(
 			array(
 				'primary' => __( 'Primary Menu', 'understrap' ),
+				'secondary' => __('Category Menu', 'understrap')
 			)
 		);
 
